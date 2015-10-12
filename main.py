e10b82b1d2b2c7df7b05dd39b1c278759c2180ca
Brandon Cercone <bcerco@gmail.com>
2015-10-12 10:02:18 -0400

    Added colors/functionality.

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ hours = {7:"8:00am", 14:"9:00am", 21:"10:00am", 28:"11:00am", 35:"12:00pm",
         42:"1:00pm", 49:"2:00pm", 56:"3:00pm", 63:"4:00pm", 70:"5:00pm", 
         77:"6:00pm", 84:"7:00pm", 91:"8:00pm", 98:"9:00pm"}
 
+def build_class(course,instructor, time_slot):
+    ret = course +"\n" +instructor+"\n"+time_slot
+    return ret
+
 class Scheduler(App):
     def build(self):
         layout = GridLayout(cols=7, rows=15)
@@ -18,13 +22,21 @@ class Scheduler(App):
                 layout.add_widget(Label(text = ""))
             elif i > 0 and i < 7:
                 name = days[i]
-                layout.add_widget(Label(text = name))
+                button = Button(text = name)
+                button.background_color = (1,1,1,0.5)
+                layout.add_widget(button)
             elif i in hours.keys():
                 name = hours[i]
                 layout.add_widget(Label(text = name))
+            elif i == 15 or i == 17 or i == 19:
+                button = Button(text=build_class("CMPSC 122","Chang","9:00-9:50am"))
+                button.background_color = (0,0,1,1)
+                layout.add_widget(button)
             else:
                 name = ""
-                layout.add_widget(Button(text = name))
+                button = Button(text = name)
+                button.background_color = (0,1,0,1)
+                layout.add_widget(button)
         
         return layout
 

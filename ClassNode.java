5784280c6f6c46e3e8f23236728d602281f48b5e
Brandon Cercone <bcerco@gmail.com>
2015-11-22 21:55:35 -0500

    Added ClassNode class.

---
+++ ClassNode.java
@@ -0,0 +1,18 @@
+//Class for class objects
+public class ClassNode{
+    private String insctructor;
+    private String course;
+    private byte number;
+    private byte section;
+    private byte credits;
+    private String title;
+    private byte soft;
+    private byte hard;
+    private byte [] days;
+    private String room;
+    private String [] times;
+    
+    ClassNode(){
+	
+    }
+}
--- ClassParser.java
+++ ClassParser.java
@@ -1,6 +1,9 @@
-public class ClassParser{
-    ClassParser(){
-    
-    }
+import java.util.*;
+import java.io.*;
 
+public class ClassParser{
+    private File inFile;
+    ClassParser(String fileToRead){
+	inFile = new File(fileToRead);
+    }
 }

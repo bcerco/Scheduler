494a116377a3d80b86a4809b44b696575d53301c
Nathan Dudding <ndudding86@gmail.com>
2015-12-01 14:05:23 -0500

    Added real time checkbox for filter.

--- GUI/StatusBar.java
+++ GUI/StatusBar.java
@@ -3,14 +3,16 @@ import java.awt.BorderLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.JCheckBox;
 
 
 public class StatusBar extends JPanel {
 	private static final long serialVersionUID = 1L;
 	
-	JPanel     filterPanel = new JPanel();
-	JLabel     filterLabel = new JLabel("Filter");
-	JTextField filterText  = new JTextField();
+	JPanel     filterPanel          = new JPanel();
+	JLabel     filterLabel          = new JLabel("Filter");
+	JTextField filterText           = new JTextField();
+    JCheckBox  filterByCharCheckbox = new JCheckBox("Apply Real Time");
 	
 	public StatusBar() {
 		Initialize();
@@ -23,6 +25,7 @@ public class StatusBar extends JPanel {
 		
 		filterPanel.add(filterLabel);
 		filterPanel.add(filterText);
+        filterPanel.add(filterByCharCheckbox);
 		this.add(filterPanel, BorderLayout.WEST);
 	}
 }

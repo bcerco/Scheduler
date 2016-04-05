9c617c62bc601e402c21dfd60ffbdac64ac944fe
Brandon Cercone <bcerco@gmail.com>
2016-04-05 14:50:13 -0400

    Handled time parsing edge case.

--- Schedule Manager/src/Class/ClassNode.java
+++ Schedule Manager/src/Class/ClassNode.java
@@ -20,298 +20,315 @@ public class ClassNode{
     public int [] endTime;
 
     public ClassNode(String line, int comma){
-	links = new ArrayList<String>();
-	String [] args = line.split(",");
-	startTime = new int[6];
-	endTime = new int[6];
-	course = args[0];
-	number = args[1];
-	section = Short.parseShort(args[2]);
-	credits = Float.parseFloat(args[3]);
-	createId();
-	switch(comma) {
-	case 10:
-	    title = args[4];
-	    try {soft = Short.parseShort(args[5]);}
-	    catch (NumberFormatException e) {soft = 0;}
-	    try {hard = Short.parseShort(args[6]);}
-	    catch (NumberFormatException e) {hard = 0;}
-	    fillDays(args[7], args[8]);
-	    room = args[9];
-	    instructor = args[10];
-	    break;
-	case 12:
-	    title = args[4];
-	    try {soft = Short.parseShort(args[5]);}
-	    catch (NumberFormatException e) {soft = 0;}
-	    try {hard = Short.parseShort(args[6]);}
-	    catch (NumberFormatException e) {hard = 0;}
-	    fillDays(args[7], args[9]);
-	    fillDays(args[8], args[10]);
-	    room = args[11];
-	    instructor = args[12];
-	    break;
-	case 13:
-	    //make sure the title it correct
-	    //might need to remove excess quotes
-	    title = args[4] + "," + args[5];
-	    try {soft = Short.parseShort(args[6]);}
-	    catch (NumberFormatException e) {soft = 0;}
-	    try {hard = Short.parseShort(args[7]);}
-	    catch (NumberFormatException e) {hard = 0;}
-	    fillDays(args[8], args[10]);
-	    fillDays(args[9], args[11]);
-	    room = args[12];
-	    instructor = args[13];
-	    break;
-	case 11:
-	    title = args[4] + "," + args[5];
-	    try {soft = Short.parseShort(args[6]);}
-	    catch (NumberFormatException e) {soft = 0;}
-	    try {hard = Short.parseShort(args[7]);}
-	    catch (NumberFormatException e) {hard = 0;}
-	    fillDays(args[8], args[9]);
-	    room = args[10];
-	    instructor = args[11];
-	    break;
-	default:
-	    break;
-	}
+        links = new ArrayList<String>();
+        String [] args = line.split(",");
+        startTime = new int[6];
+        endTime = new int[6];
+        course = args[0];
+        number = args[1];
+        section = Short.parseShort(args[2]);
+        credits = Float.parseFloat(args[3]);
+        createId();
+        switch(comma) {
+            case 10:
+                title = args[4];
+                try {soft = Short.parseShort(args[5]);}
+                catch (NumberFormatException e) {soft = 0;}
+                try {hard = Short.parseShort(args[6]);}
+                catch (NumberFormatException e) {hard = 0;}
+                fillDays(args[7], args[8]);
+                room = args[9];
+                instructor = args[10];
+                break;
+            case 12:
+                title = args[4];
+                try {soft = Short.parseShort(args[5]);}
+                catch (NumberFormatException e) {soft = 0;}
+                try {hard = Short.parseShort(args[6]);}
+                catch (NumberFormatException e) {hard = 0;}
+                fillDays(args[7], args[9]);
+                fillDays(args[8], args[10]);
+                room = args[11];
+                instructor = args[12];
+                break;
+            case 13:
+                //make sure the title it correct
+                //might need to remove excess quotes
+                title = args[4] + "," + args[5];
+                try {soft = Short.parseShort(args[6]);}
+                catch (NumberFormatException e) {soft = 0;}
+                try {hard = Short.parseShort(args[7]);}
+                catch (NumberFormatException e) {hard = 0;}
+                fillDays(args[8], args[10]);
+                fillDays(args[9], args[11]);
+                room = args[12];
+                instructor = args[13];
+                break;
+            case 11:
+                title = args[4] + "," + args[5];
+                try {soft = Short.parseShort(args[6]);}
+                catch (NumberFormatException e) {soft = 0;}
+                try {hard = Short.parseShort(args[7]);}
+                catch (NumberFormatException e) {hard = 0;}
+                fillDays(args[8], args[9]);
+                room = args[10];
+                instructor = args[11];
+                break;
+            default:
+                break;
+        }
 
     }
     public String exportClassNode(){
-	generateLinks();
-	String ret = "";
-	ret += course + "," + number + "," + exportSection() + "," + credits + "," + exportTitle() + "," +
-	    soft + "," + hard + "," + exportDayTime() + "," + room + "," + instructor + "\n";
-	return ret;
+        generateLinks();
+        String ret = "";
+        ret += course + "," + number + "," + exportSection() + "," + credits + "," + exportTitle() + "," +
+            soft + "," + hard + "," + exportDayTime() + "," + room + "," + instructor + "\n";
+        return ret;
     }
     public String exportSection(){
-    	if (section < 10)
-    		return "00" + Short.toString(section);
-    	else
-    		return "0" + Short.toString(section);
+        if (section < 10)
+            return "00" + Short.toString(section);
+        else
+            return "0" + Short.toString(section);
     }
     public String exportTitle(){
-	if (title.contains(",")){
-	    String [] sp = title.split(",");
-	    return  sp[0] + "\n" + sp[1];
-	}
-	else{
-	    return title;
-	}
+        if (title.contains(",")){
+            String [] sp = title.split(",");
+            return  sp[0] + "\n" + sp[1];
+        }
+        else{
+            return title;
+        }
     }
     public String exportDayTime(){
-	String ret = "";
-	Iterator<String> iter = links.iterator();
-	if (links.size() == 1){
-	    String link = iter.next();
-	    int index = dayToInt(link.charAt(0));
-	    ret += link + "," + timeToString(startTime[index], endTime[index]);
-	}
-	else{
-	    String link = iter.next();
-	    int index1 = dayToInt(link.charAt(0));
-	    ret += "\"" + link + "\n";
-	    link = iter.next();
-	    int index2 = dayToInt(link.charAt(0));
-	    ret += link + "\",\"" + timeToString(startTime[index1], endTime[index1]) + "\n";
-	    ret += timeToString(startTime[index2], endTime[index2]) + "\"";
-	}
-	return ret;
+        String ret = "";
+        Iterator<String> iter = links.iterator();
+        if (links.size() == 1){
+            String link = iter.next();
+            int index = dayToInt(link.charAt(0));
+            ret += link + "," + timeToString(startTime[index], endTime[index]);
+        }
+        else{
+            String link = iter.next();
+            int index1 = dayToInt(link.charAt(0));
+            ret += "\"" + link + "\n";
+            link = iter.next();
+            int index2 = dayToInt(link.charAt(0));
+            ret += link + "\",\"" + timeToString(startTime[index1], endTime[index1]) + "\n";
+            ret += timeToString(startTime[index2], endTime[index2]) + "\"";
+        }
+        return ret;
     }
     public void generateLinks(){
-	links.clear();
-	HashSet<Character> visited = new HashSet<Character>();
-	for (int i = 0; i < 6; i++) {
-	    if (!visited.contains(intToDay(i)) && startTime[i] != 0){
-		String link = "";
-		link += intToDay(i);
-		visited.add(intToDay(i));
-		for (int j = 0; j < 6; j++) {
-		    if (i != j){
-			if (startTime[i] == startTime[j] && endTime[i] == endTime[j]){
-			    link += intToDay(j);
-			    visited.add(intToDay(j));
-			}
-		    }
-		}
-		links.add(link);
-	    }
-	}
+        links.clear();
+        HashSet<Character> visited = new HashSet<Character>();
+        for (int i = 0; i < 6; i++) {
+            if (!visited.contains(intToDay(i)) && startTime[i] != 0){
+                String link = "";
+                link += intToDay(i);
+                visited.add(intToDay(i));
+                for (int j = 0; j < 6; j++) {
+                    if (i != j){
+                        if (startTime[i] == startTime[j] && endTime[i] == endTime[j]){
+                            link += intToDay(j);
+                            visited.add(intToDay(j));
+                        }
+                    }
+                }
+                links.add(link);
+            }
+        }
     }
     public String timeToString(int sTime, int eTime){
-	int shour = sTime / 60;
-	int sminutes = sTime - (shour * 60);
-	int ehour = eTime / 60;
-	int eminutes = eTime - (ehour * 60);
-	String end = "";
-	if (shour > 12)
-	    shour-= 12;
-	if (ehour > 12){
-	    ehour-=12;
-	    end = "P";
-	}
-	else
-	    end = "A";
-	String ret = Integer.toString(shour) + ":";
-	if (sminutes < 10){
-	    ret += "0" + Integer.toString(sminutes);
-	}
-	else{
-	    ret += Integer.toString(sminutes);
-	}
-	ret += "-" + Integer.toString(ehour) + ":";
-	if (eminutes < 10){
-	    ret += "0" + Integer.toString(eminutes);
-	}
-	else{
-	    ret += Integer.toString(eminutes);
-	}
-	ret += end;
-	return ret;
+        int shour = sTime / 60;
+        int sminutes = sTime - (shour * 60);
+        int ehour = eTime / 60;
+        int eminutes = eTime - (ehour * 60);
+        String end = "";
+        if (shour > 12)
+            shour-= 12;
+        if (ehour > 12){
+            ehour-=12;
+            end = "P";
+        }
+        else
+            end = "A";
+        String ret = Integer.toString(shour) + ":";
+        if (sminutes < 10){
+            ret += "0" + Integer.toString(sminutes);
+        }
+        else{
+            ret += Integer.toString(sminutes);
+        }
+        ret += "-" + Integer.toString(ehour) + ":";
+        if (eminutes < 10){
+            ret += "0" + Integer.toString(eminutes);
+        }
+        else{
+            ret += Integer.toString(eminutes);
+        }
+        ret += end;
+        return ret;
     }
     public char intToDay(int i){
-	switch(i){
-	case 0:
-	    return 'M';
-	case 1:
-	    return 'T';
-	case 2:
-	    return 'W';
-	case 3:
-	    return 'R';
-	case 4:
-	    return 'F';
-	case 5:
-	    return 'S';
-	case 6:
-	    return 'U';
-	default:
-	    return 'E';
-	}
+        switch(i){
+            case 0:
+                return 'M';
+            case 1:
+                return 'T';
+            case 2:
+                return 'W';
+            case 3:
+                return 'R';
+            case 4:
+                return 'F';
+            case 5:
+                return 'S';
+            case 6:
+                return 'U';
+            default:
+                return 'E';
+        }
     }
     public int dayToInt(char d){
-	switch(d){
-	case 'M':
-	    return 0;
-	case 'T':
-	    return 1;
-	case 'W':
-	    return 2;
-	case 'R':
-	    return 3;
-	case 'F':
-	    return 4;
-	case 'S':
-	    return 5;
-	case 'U':
-	    return 6;
-	default:
-	    return -1;
-	}
+        switch(d){
+            case 'M':
+                return 0;
+            case 'T':
+                return 1;
+            case 'W':
+                return 2;
+            case 'R':
+                return 3;
+            case 'F':
+                return 4;
+            case 'S':
+                return 5;
+            case 'U':
+                return 6;
+            default:
+                return -1;
+        }
     }
     public void createId(){
-	id = course + number + section;
+        id = course + number + section;
     }
     public String getId(){
-	return id;
+        return id;
     }
     //TODO: add something for am/pm
     public void fillDays(String day, String time){
-	day = day.replace("\"","");
-	time = time.replace("\"","");
-	time = time.replace(" ","");
-	String [] times = time.split("-");
-	//System.out.println(times[0]+times[1]);
-	int start = (Integer.parseInt(times[0].split(":")[0]) * 60) +
-	    Integer.parseInt(times[0].split(":")[1]);
-	int end = (Integer.parseInt(times[1].split(":")[0]) * 60) +
-	    Integer.parseInt(times[1].split(":")[1].substring(0,2));
-	if (times[1].split(":")[1].charAt(2) == 'P'){
-	    if( end < 720) end += 720;
-	    if (start < 540) start += 720;
-	}
-	//System.out.printf("%d, %d\n",start,end);
-	for (int i = 0; i < day.length(); i++) {
-	    switch(day.charAt(i)){
-	    case 'M':
-		startTime[0] = start;
-		endTime[0] = end;
-		break;
-	    case 'T':
-		startTime[1] = start;
-		endTime[1] = end;
-		break;
-	    case 'W':
-		startTime[2] = start;
-		endTime[2] = end;
-		break;
-	    case 'R':
-		startTime[3] = start;
-		endTime[3] = end;
-		break;
-	    case 'F':
-		startTime[4] = start;
-		endTime[4] = end;
-		break;
-	    case 'S':
-		startTime[5] = start;
-		endTime[5] = end;
-		break;
-	    }
-	}
+        int start, end;
+        day = day.replace("\"","");
+        time = time.replace("\"","");
+        time = time.replace(" ","");
+        String [] times = time.split("-");
+        //System.out.println(times[0]+times[1]);
+        if (times[0].contains(":")){
+            start = (Integer.parseInt(times[0].split(":")[0]) * 60) +
+                Integer.parseInt(times[0].split(":")[1]);
+        }
+        else{
+            start = Integer.parseInt(times[0]) * 60;
+        }
+        if (times[1].contains(":")){
+            end = (Integer.parseInt(times[1].split(":")[0]) * 60) +
+                Integer.parseInt(times[1].split(":")[1].substring(0,2));
+        }
+        else{
+            end = Integer.parseInt(times[1].substring(0, 
+                        times[1].length() - 1)) * 60;
+        }/*
+        if (times[1].split(":")[1].charAt(2) == 'P'){
+            if (end < 720) end += 720;
+            if (start < 540) start += 720;
+        }
+        */
+        if (times[1].charAt(times[1].length() - 1) == 'P'){
+            if (end < 720) end += 720;
+            if (start < 540) start += 720;
+        }
+        //System.out.printf("%d, %d\n",start,end);
+        for (int i = 0; i < day.length(); i++) {
+            switch(day.charAt(i)){
+                case 'M':
+                    startTime[0] = start;
+                    endTime[0] = end;
+                    break;
+                case 'T':
+                    startTime[1] = start;
+                    endTime[1] = end;
+                    break;
+                case 'W':
+                    startTime[2] = start;
+                    endTime[2] = end;
+                    break;
+                case 'R':
+                    startTime[3] = start;
+                    endTime[3] = end;
+                    break;
+                case 'F':
+                    startTime[4] = start;
+                    endTime[4] = end;
+                    break;
+                case 'S':
+                    startTime[5] = start;
+                    endTime[5] = end;
+                    break;
+            }
+        }
     }
     //This returns the interger array of start times, in minutes
     public int[] getStartTime(){
-	return startTime;
+        return startTime;
     }
     //This returns the integer array of end times, in minutes
     public int[] getEndTime(){
-	return endTime;
+        return endTime;
     }
     public void setInstructor(String ins, HashMap<String, HashSet<String> > instructorList){
-	//TODO: handle instructor change
-	/*
-	instructorList.get(instructor).remove(id);
-	instructorList.get(ins).add(id);
-	*/
-	instructor = ins;
+        //TODO: handle instructor change
+        /*
+           instructorList.get(instructor).remove(id);
+           instructorList.get(ins).add(id);
+           */
+        instructor = ins;
     }
     public String getInstructor(){
-	return instructor;
+        return instructor;
     }
     public String getRoom(){
-	return room;
+        return room;
     }
     public void setRoom(String r){
-	room = r;
+        room = r;
     }
     public String getTitle(){
-	return title;
+        return title;
     }
     public void setTitle(String t){
-	title = t;
+        title = t;
     }
     public String getCourse() {
-    	return course;
+        return course;
     }
     public String getNumber() {
-    	return number;
+        return number;
     }
     public short getSection() {
-	return section;
+        return section;
     }
     public short getHard(){
-	return hard;
+        return hard;
     }
     public short getSoft(){
-	return soft;
+        return soft;
     }
     public void setHard(short h){
-	hard = h;
+        hard = h;
     }
     public void setSoft(short s){
-	soft = s;
+        soft = s;
     }
 }
